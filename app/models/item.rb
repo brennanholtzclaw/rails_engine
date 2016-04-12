04336bb0e58a426b78253d0b9c5a21cb2329e38a
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-12 16:58:05 -0600

    Migrate Item Unit Price to Decimal

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ActiveRecord::Base
+  before_validation :convert_unit_price
+
   belongs_to :merchant
+
+  def convert_unit_price
+    self.unit_price = (unit_price.to_f / 100)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412221339) do
+ActiveRecord::Schema.define(version: 20160412224419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20160412221339) do
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.integer  "unit_price"
+    t.decimal  "unit_price"
     t.integer  "merchant_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
