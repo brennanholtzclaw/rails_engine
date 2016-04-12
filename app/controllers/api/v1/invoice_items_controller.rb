1ad460457f23760a5d83f5cad37117573ccc46e7
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-11 23:07:03 -0600

    Pass Request Template for invoice_items endpoints

---
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::InvoiceItemsController < ApiController
+  respond_to :json
+
+  def show
+    respond_with InvoiceItem.find(params[:id])
+  end
+
+  def index
+    respond_with InvoiceItem.all
+  end
+
+  def find
+    respond_with InvoiceItem.find_by(params.keys[0] => params.values[0])
+  end
+
+  def find_all
+    respond_with InvoiceItem.where(params.keys[0] => params.values[0])
+  end
+
+end
--- spec/requests/api/v1/invoice_items_spec.rb
+++ spec/requests/api/v1/invoice_items_spec.rb
@@ -43,91 +43,133 @@ RSpec.describe "GET /api/v1/invoice_items" do
     expect(parsed_response[2]["id"]).to eq(invoice_item3.id)
   end
 
-  # it "returns just one invoice_item by id" do
-  #   merchant1 = create(:merchant, name: "Brennan")
-  #   merchant2 = create(:merchant, name: "John")
-  #   customer1 = create(:customer, first_name: "Brennan", last_name: "Doe")
-  #   customer2 = create(:customer, first_name: "John", last_name: "Doe")
-  #   invoice_item1 = create(:invoice_item, customer_id: customer1.id, merchant_id: merchant1.id)
-  #   invoice_item2 = create(:invoice_item, customer_id: customer2.id, merchant_id: merchant1.id)
-  #   invoice_item3 = create(:invoice_item, customer_id: customer1.id, merchant_id: merchant1.id, status: "Paid")
-  #
-  #   get "/api/v1/invoice_items/#{invoice_item1.id}"
-  #
-  #   expect(parsed_response).to eq({
-  #     "id"         => invoice_item1.id,
-  #     "customer_id" => invoice_item1.customer_id,
-  #     "merchant_id" => invoice_item1.merchant_id,
-  #     "status" => invoice_item1.status,
-  #     "created_at" => format_date(invoice_item1.created_at),
-  #     "updated_at" => format_date(invoice_item1.updated_at)
-  #   })
-  #
-  #   expect(parsed_response).to_not include(invoice_item2.id)
-  #   # expect(parsed_response).to_not include(JSON.parse(invoice_item3.to_json)) #many PIECES are included - RSpec doesn't like it
-  # end
-  #
-  # it "returns just one invoice_item by any criteria" do
-  #   merchant1 = create(:merchant, name: "Brennan")
-  #   merchant2 = create(:merchant, name: "John")
-  #   customer1 = create(:customer, first_name: "Brennan", last_name: "Doe")
-  #   customer2 = create(:customer, first_name: "John", last_name: "Doe")
-  #   invoice_item1 = create(:invoice_item, customer_id: customer1.id, merchant_id: merchant1.id)
-  #   invoice_item2 = create(:invoice_item, customer_id: customer2.id, merchant_id: merchant1.id)
-  #   invoice_item3 = create(:invoice_item, customer_id: customer1.id, merchant_id: merchant1.id, status: "Paid")
-  #
-  #
-  #   get "/api/v1/invoice_items/find?id=#{invoice_item1.id}"
-  #
-  #   expect(parsed_response).to eq({
-  #     "id"         => invoice_item1.id,
-  #     "customer_id" => invoice_item1.customer_id,
-  #     "merchant_id" => invoice_item1.merchant_id,
-  #     "status" => invoice_item1.status,
-  #     "created_at" => format_date(invoice_item1.created_at),
-  #     "updated_at" => format_date(invoice_item1.updated_at)
-  #   })
-  #
-  #   get "/api/v1/invoice_items/find?status=#{invoice_item3.status}"
-  #
-  #   expect(parsed_response).to eq({
-  #     "id"         => invoice_item3.id,
-  #     "customer_id" => invoice_item3.customer_id,
-  #     "merchant_id" => invoice_item3.merchant_id,
-  #     "status" => invoice_item3.status,
-  #     "created_at" => format_date(invoice_item3.created_at),
-  #     "updated_at" => format_date(invoice_item3.updated_at)
-  #   })
-  # end
-  #
-  # it "returns all invoice_items by any criteria" do
-  #   merchant1 = create(:merchant, name: "Brennan")
-  #   merchant2 = create(:merchant, name: "John")
-  #   customer1 = create(:customer, first_name: "Brennan", last_name: "Doe")
-  #   customer2 = create(:customer, first_name: "John", last_name: "Doe")
-  #   invoice_item1 = create(:invoice_item, customer_id: customer1.id, merchant_id: merchant1.id)
-  #   invoice_item2 = create(:invoice_item, customer_id: customer2.id, merchant_id: merchant1.id)
-  #   invoice_item3 = create(:invoice_item, customer_id: customer1.id, merchant_id: merchant1.id, status: "Paid")
-  #
-  #   get "/api/v1/invoice_items/find_all?id=#{invoice_item1.id}"
-  #
-  #   expect(parsed_response[0]).to eq({
-  #     "id"         => invoice_item1.id,
-  #     "customer_id" => invoice_item1.customer_id,
-  #     "merchant_id" => invoice_item1.merchant_id,
-  #     "status" => invoice_item1.status,
-  #     "created_at" => format_date(invoice_item1.created_at),
-  #     "updated_at" => format_date(invoice_item1.updated_at)
-  #   })
-  #
-  #   get "/api/v1/invoice_items/find_all?customer_id=#{invoice_item1.customer_id}"
-  #
-  #   expect(parsed_response.count).to eq(2)
-  #
-  #   expect(parsed_response.first["customer_id"]).to eq(invoice_item1.customer_id)
-  #   expect(parsed_response.first["status"]).to eq(invoice_item1.status)
-  #   expect(parsed_response.last["customer_id"]).to eq(invoice_item3.customer_id)
-  #   expect(parsed_response.last["status"]).to eq(invoice_item3.status)
-  #   expect(parsed_response).to_not include(JSON.parse(invoice_item2.to_json))
-  # end
+  it "returns just one invoice_item by id" do
+    merchant1 = create(:merchant, name: "Brennan")
+    merchant2 = create(:merchant, name: "John")
+    customer1 = create(:customer, first_name: "Brennan", last_name: "Doe")
+    customer2 = create(:customer, first_name: "John", last_name: "Doe")
+    item1 = create(:item, merchant_id: merchant1.id)
+    item2 = create(:item, merchant_id: merchant2.id)
+    item3 = create(:item, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id)
+    invoice2 = create(:invoice, customer_id: customer2.id, merchant_id: merchant1.id)
+    invoice3 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id, status: "Paid")
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id)
+    invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id)
+    invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id)
+
+    get "/api/v1/invoice_items/#{invoice_item1.id}"
+
+    expect(parsed_response).to eq({
+      "id"         => invoice_item1.id,
+      "item_id"    => invoice_item1.item_id,
+      "invoice_id" => invoice_item1.invoice_id,
+      "quantity"   => invoice_item1.quantity,
+      "unit_price" => invoice_item1.unit_price,
+      "created_at" => format_date(invoice_item1.created_at),
+      "updated_at" => format_date(invoice_item1.updated_at)
+    })
+
+    expect(parsed_response).to_not include(invoice_item2.id)
+  end
+
+  it "returns just one invoice_item by any criteria" do
+    merchant1 = create(:merchant, name: "Brennan")
+    merchant2 = create(:merchant, name: "John")
+    customer1 = create(:customer, first_name: "Brennan", last_name: "Doe")
+    customer2 = create(:customer, first_name: "John", last_name: "Doe")
+    item1 = create(:item, merchant_id: merchant1.id)
+    item2 = create(:item, merchant_id: merchant2.id)
+    item3 = create(:item, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id)
+    invoice2 = create(:invoice, customer_id: customer2.id, merchant_id: merchant1.id)
+    invoice3 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id, status: "Paid")
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id)
+    invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, unit_price: 999)
+    invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 888)
+
+    get "/api/v1/invoice_items/find?id=#{invoice_item1.id}"
+
+    expect(parsed_response).to eq({
+      "id"         => invoice_item1.id,
+      "item_id"    => invoice_item1.item_id,
+      "invoice_id" => invoice_item1.invoice_id,
+      "quantity"   => invoice_item1.quantity,
+      "unit_price" => invoice_item1.unit_price,
+      "created_at" => format_date(invoice_item1.created_at),
+      "updated_at" => format_date(invoice_item1.updated_at)
+    })
+
+    get "/api/v1/invoice_items/find?unit_price=#{invoice_item2.unit_price}"
+
+    expect(parsed_response).to eq({
+      "id"         => invoice_item2.id,
+      "item_id"    => invoice_item2.item_id,
+      "invoice_id" => invoice_item2.invoice_id,
+      "quantity"   => invoice_item2.quantity,
+      "unit_price" => invoice_item2.unit_price,
+      "created_at" => format_date(invoice_item2.created_at),
+      "updated_at" => format_date(invoice_item2.updated_at)
+    })
+
+    get "/api/v1/invoice_items/find?quantity=#{invoice_item3.quantity}"
+
+    expect(parsed_response).to eq({
+      "id"         => invoice_item3.id,
+      "item_id"    => invoice_item3.item_id,
+      "invoice_id" => invoice_item3.invoice_id,
+      "quantity"   => invoice_item3.quantity,
+      "unit_price" => invoice_item3.unit_price,
+      "created_at" => format_date(invoice_item3.created_at),
+      "updated_at" => format_date(invoice_item3.updated_at)
+    })
+  end
+
+  it "returns all invoice_items by any criteria" do
+    merchant1 = create(:merchant, name: "Brennan")
+    merchant2 = create(:merchant, name: "John")
+    customer1 = create(:customer, first_name: "Brennan", last_name: "Doe")
+    customer2 = create(:customer, first_name: "John", last_name: "Doe")
+    item1 = create(:item, merchant_id: merchant1.id)
+    item2 = create(:item, merchant_id: merchant2.id)
+    item3 = create(:item, merchant_id: merchant1.id)
+    invoice1 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id)
+    invoice2 = create(:invoice, customer_id: customer2.id, merchant_id: merchant1.id)
+    invoice3 = create(:invoice, customer_id: customer1.id, merchant_id: merchant1.id, status: "Paid")
+    invoice_item1 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id)
+    invoice_item2 = create(:invoice_item, item_id: item2.id, invoice_id: invoice2.id, quantity: 45)
+    invoice_item3 = create(:invoice_item, item_id: item1.id, invoice_id: invoice1.id, quantity: 45)
+
+    get "/api/v1/invoice_items/find_all?id=#{invoice_item1.id}"
+
+    expect(parsed_response[0]).to eq({
+      "id"         => invoice_item1.id,
+      "item_id"    => invoice_item1.item_id,
+      "invoice_id" => invoice_item1.invoice_id,
+      "quantity"   => invoice_item1.quantity,
+      "unit_price" => invoice_item1.unit_price,
+      "created_at" => format_date(invoice_item1.created_at),
+      "updated_at" => format_date(invoice_item1.updated_at)
+    })
+
+    get "/api/v1/invoice_items/find_all?item_id=#{invoice_item1.item_id}"
+
+    expect(parsed_response.count).to eq(2)
+
+    expect(parsed_response.first["invoice_id"]).to eq(invoice_item1.invoice_id)
+    expect(parsed_response.first["quantity"]).to eq(invoice_item1.quantity)
+    expect(parsed_response.last["invoice_id"]).to eq(invoice_item3.invoice_id)
+    expect(parsed_response.last["quantity"]).to eq(invoice_item3.quantity)
+    expect(parsed_response).to_not include(JSON.parse(invoice_item2.to_json))
+
+    get "/api/v1/invoice_items/find_all?quantity=#{invoice_item2.quantity}"
+
+    expect(parsed_response.count).to eq(2)
+
+    expect(parsed_response.first["invoice_id"]).to eq(invoice_item2.invoice_id)
+    expect(parsed_response.first["quantity"]).to eq(invoice_item2.quantity)
+    expect(parsed_response.last["invoice_id"]).to eq(invoice_item3.invoice_id)
+    expect(parsed_response.last["quantity"]).to eq(invoice_item3.quantity)
+    expect(parsed_response).to_not include(JSON.parse(invoice_item1.to_json))
+  end
 end
