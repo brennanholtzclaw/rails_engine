ae4139fc8718bde07b95eb3599352d815cf1603a
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-04-12 22:16:40 -0600

    This was an aliased commit.

--- spec/requests/api/v1/relationships/item_relationships_spec.rb
+++ spec/requests/api/v1/relationships/item_relationships_spec.rb
@@ -9,27 +9,34 @@ RSpec.describe "GET /api/v1/item relationships" do
     date.strftime("%Y-%m-%dT%H:%M:%S.%LZ")
   end
 
-  it "returns associated invoice_item invoice" do
-    # GET /api/v1/invoice_items/:id/invoice returns the associated invoice
+
+  it "returns a collection of item invoice_items" do
+    # GET /api/v1/items/:id/invoice_items returns a collection of associated invoice items
+    item = create(:item)
+    invoice1 = create(:invoice)
+    invoice2 = create(:invoice)
     invoice_item1 = create(:invoice_item)
     invoice_item2 = create(:invoice_item)
-    invoice = create(:invoice)
-    invoice.invoice_items << invoice_item1
-    invoice.invoice_items << invoice_item2
+    invoice_item1.update(item: item)
+    invoice_item2.update(item: item)
+    invoice1.invoice_items << invoice_item1
+    invoice2.invoice_items << invoice_item2
 
-    get "/api/v1/invoice_items/#{invoice_item1.id}/invoice"
+    get "/api/v1/items/#{item.id}/invoice_items"
 
-    expect(parsed_response).to eq(JSON.parse(invoice.to_json))
+    expect(parsed_response.count).to eq(2)
+    expect(parsed_response[0]).to eq(JSON.parse(invoice_item1.to_json))
+    expect(parsed_response[1]["status"]).to eq(invoice_item2.status)
   end
 
-  it "returns associated invoice_item item" do
-    # GET /api/v1/invoice_items/:id/item returns the associated item
-    invoice_item = create(:invoice_item)
-    item = create(:item)
-    invoice_item.update(item: item)
-
-    get "/api/v1/invoice_items/#{invoice_item.id}/item"
-
-    expect(parsed_response).to eq(JSON.parse(item.to_json))
-  end
+  # it "returns associated item merchant" do
+  #   # GET /api/v1/items/:id/merchant returns the associated merchant
+  #   invoice_item = create(:invoice_item)
+  #   item = create(:item)
+  #   invoice_item.update(item: item)
+  #
+  #   get "/api/v1/invoice_items/#{invoice_item.id}/item"
+  #
+  #   expect(parsed_response).to eq(JSON.parse(item.to_json))
+  # end
 end
